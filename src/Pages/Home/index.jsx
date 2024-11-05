93f94d902d0882fe3a6f1e9b0205c311b30329c2
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-11-05 17:33:25 -0500

    Card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Layout } from "../../Components/Layout"
+import { Card } from "../../Components/Card"
 
 function Home(){
   return (
     <Layout>
       <h1>Home</h1>
+      <Card />
     </Layout>
   )
 }
