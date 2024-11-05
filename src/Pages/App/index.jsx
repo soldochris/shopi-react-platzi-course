e0282f418e4a0c4a5d77628dd30c9c299a324bd2
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-11-05 12:00:50 -0500

    routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import {Home} from '../Home'
 import {MyAccount} from '../MyAccount'
 import {MyOrder} from '../MyOrder'
@@ -6,17 +7,25 @@ import {NotFound} from '../NotFound'
 import {SignIn} from '../SignIn'
 import './App.css'
 
+function AppRoutes(){
+  let routes = useRoutes([
+    { path: '/', element: <Home/> },
+    { path: '/my-account', element: <MyAccount/> },
+    { path: '/my-order', element: <MyOrder/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/sign-in', element: <SignIn/> },
+    { path: '/*', element: <NotFound/> }
+  ])
+
+  return routes
+}
+
 function App() {
 
   return (
-    <>
-      <Home/>
-      <MyAccount/>
-      <MyOrder/>
-      <MyOrders/>
-      <NotFound/>
-      <SignIn/>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
