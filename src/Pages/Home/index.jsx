91af8f62d6a9e7862ab0ac94e11783878a370c04
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-11-11 12:15:33 -0500

    consuming API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 
 function Home(){
+
+  const [items, setItems] = useState(null)
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
       <h1>Home</h1>
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map( (item) => (
+            <Card data={item} key={item.id}/>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
