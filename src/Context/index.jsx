e54f87f6cb78b4b361e62ab59c67905323c114b0
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-11-11 17:03:17 -0500

    context

---
+++ src/Context/index.jsx
@@ -0,0 +1,13 @@
+import { createContext  } from "react"
+
+const ShoppingCartContext = createContext()
+
+function ShoppingCartProvider({children}){
+  return(
+    <ShoppingCartContext.Provider>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
+
+export {ShoppingCartProvider}
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import {Home} from '../Home'
 import {MyAccount} from '../MyAccount'
 import {MyOrder} from '../MyOrder'
@@ -24,10 +25,12 @@ function AppRoutes(){
 function App() {
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRoutes />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
